73aea08d4ce64cb4b598cd31a2216e932eec42f6
gagandamade <gagandamade@gmail.com>
2022-03-01 10:08:48 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import faker from "faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart"
+    }
+  }
+};
+
+const labels = ["January"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(2, 87, 0, 0.5)"
+    }
+  ]
+};
+
+export function App() {
+  return <Bar options={options} data={data} />;
+}
